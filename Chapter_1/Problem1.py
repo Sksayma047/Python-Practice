2e488c99744c2acd25b8feb9cd2eabc0104493d1
Sayma <skcsayma02@gmail.com>
2026-07-16 00:35:46 +0530

    added txt file

--- Chapter_1/Problem1.py
+++ Chapter_1/Problem1.py
@@ -6,3 +6,11 @@ engine = pyttsx3.init()
 
 engine.say("I will speak this text")
 engine.runAndWait()
+
+
+string = input("Enter you string")
+for ch in string.lower():
+    if ch in "aeiou":
+        count += 1
+print("Number of vowels is: ", count)
+
